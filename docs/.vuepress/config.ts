04bf1b95ea87a9154b5160d2c6d3910bf476c231
CZB <1182733571@qq.com>
2024-09-28 18:40:22 +0800

    添加内容Docker 数据管理：备份和恢复指南.md

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -66,21 +66,32 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
               },
             ],
           },
+          {
+            text: '页面',
+            link: '/ui/',
+            items: [
+              {text: 'HTML', link: '/pages/8309a5b876fc95e3/'},
+              {text: 'CSS', link: '/pages/0a83b083bdf257cb/'},
+            ],
+          },
         ],
       },
-      {
-        text: '页面',
-        link: '/ui/',
-        items: [
-          { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
-        ],
-      },
+
       {
         text: '技术',
         link: '/technology/',
         items: [
           { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+        ],
+      },
+      {
+        text: '运维',
+        link: '/maintenance/',
+        items: [
+          { text: 'Docker', link: '/pages/c105e8/' },
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
